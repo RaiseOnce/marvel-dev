d9e5a36a435b44652478ab6bde8b9cf13eec9d3c
Otto Rahn <elias.yunusoff@gmail.com>
2023-07-31 17:34:17 +0500

    changes

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,31 +1,31 @@
 class MarvelService {
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=37e3f8e34073086314dd008a55fef3c7';
-  _baseOffset = 210;
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+  _apiKey = 'apikey=37e3f8e34073086314dd008a55fef3c7'
+  _baseOffset = 210
 
   getResource = async (url) => {
-    let res = await fetch(url);
+    let res = await fetch(url)
 
     if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
     }
 
-    return await res.json();
-  };
+    return await res.json()
+  }
 
   getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
-    );
-    return res.data.results.map(this._transformCharacter);
-  };
+    )
+    return res.data.results.map(this._transformCharacter)
+  }
 
   getCharacter = async (id) => {
     const res = await this.getResource(
       `${this._apiBase}characters/${id}?&${this._apiKey}`
-    );
-    return this._transformCharacter(res.data.results[0]);
-  };
+    )
+    return this._transformCharacter(res.data.results[0])
+  }
 
   _transformCharacter = (char) => {
     return {
@@ -38,8 +38,8 @@ class MarvelService {
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
       comics: char.comics.items,
-    };
-  };
+    }
+  }
 }
 
-export default MarvelService;
+export default MarvelService
